c6c388c3ed5a1286b21129413edcc8365c515a40
EduMelo <dev@edumelo.com.br>
2023-05-06 15:48:14 -0300

    mudada enderço do back

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
    	const [data, setData] = useState([]);
 
 	useEffect(() => {
-		fetch("$BACKEND_URL")
+		fetch("https://api-grupo09-prod-bmhkhouqrq-uc.a.run.app/")
 		 .then((res) => res.json())
       		 .then((result) => setData(result))
       		 .catch((err) => console.log("error"));
